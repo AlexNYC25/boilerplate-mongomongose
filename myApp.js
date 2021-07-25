8547c57c990d187b552ab832a29e5acecf2c2c47
Alexis Montes <montes.alexis25@gmail.com>
2021-07-25 16:19:56 -0400

    changed exec chain

--- myApp.js
+++ myApp.js
@@ -70,7 +70,9 @@ const removeManyPeople = (done) => {
 const queryChain = (done) => {
   const foodToSearch = "burrito";
 
-  Person.find({favoriteFoods: foodToSearch}).sort({name: 1}).limit(2).select({age:0}).exec(done(err, data));
+  Person.find({favoriteFoods: foodToSearch}).sort({name: 1}).limit(2).select({age:0}).exec((err, data) => {
+    (err) ? done(err) : done(null, data); 
+  })
 
 };
 
